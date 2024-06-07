b7bf24b63104298b85b7e3e9bacd3ed54086c781
aleenah <xpeditioner@gmail.com>
2024-06-07 16:02:44 -0400

    fixed borrowable

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -4,5 +4,5 @@ values (1, 'harry potter and the prisoner of azkaban', 'Fantasy', 'J.K Rowling')
 insert into book (id, title, genre, author)
 values (2, 'Learn AWS', 'Education', 'Lucia Lee');
 
-insert into periodical (id, title, genre, author, isBorrowable)
+insert into periodical (id, title, genre, author, borrowable)
 values (3, 'testPeriodical', 'Education', 'John Doe', false);
